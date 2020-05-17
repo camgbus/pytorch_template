bdb3f3847398b724d7aea9665572f611bbb81417
camgbus <camilag.bustillo@gmail.com>
2020-05-17 12:34:08 +0200

    Finished notebook

--- ptt/cifar_10_experiment_example.py
+++ ptt/cifar_10_experiment_example.py
@@ -1,6 +1,9 @@
-import os
+# ------------------------------------------------------------------------------
+# An example equivalent to cifar_10_experiment_notebook bot using the
+# experiment formatting and cross-validation, not using hold-out data.
+# ------------------------------------------------------------------------------
+
 import torch
-assert torch.cuda.is_available()
 import torch.nn as nn
 import torch.optim as optim
 
@@ -10,6 +13,31 @@ from ptt.data.pytorch_dataset import ImgClassificationDataset
 from ptt.agents.classification_agent import ClassificationAgent
 from ptt.eval.result import Result
 from ptt.visualization.plot_results import plot_results
+
+config = {'batch_size':32, 'lr':1e-3, 'momentum':0.9, 'device':'cuda:1', 'nr_epochs': 5, 'tracking_interval': 1}
+
+def run():
+    # Fetch data, transform to PyTorch format and build dataloaders
+    data = CIFAR10()
+    datasets = {'train': ImgClassificationDataset(data, ix_lst=None, resize=None, norm=data.x_norm),
+        'test': ImgClassificationDataset(data, ix_lst=data.hold_out_ixs, resize=None, norm=data.x_norm)}
+    dataloaders = dict()
+    for split, ds in datasets.items():
+        shuffle = not(split == 'test')
+        dataloaders[split] = torch.utils.data.DataLoader(ds, batch_size=config['batch_size'], shuffle=shuffle)
+
+    # Get model
+    model = SmallCNN(input_shape=data.input_shape, output_shape=data.output_shape)
+    model.to(config['device'])
+
+    # Define criterion and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=config['lr'], momentum=config['momentum'])
+
+    # Train model
+    results = Result(name='training_trajectory')
+    agent = ClassificationAgent(config=config, base_criterion=criterion, verbose=True)
+    agent.train(results, model, optimizer, trainloader=dataloaders['train'], dataloaders=dataloaders)
 
 
 
