b911905247c5dbfcf0bdb8a9f7f11c73b17e4002
camgbus <camilag.bustillo@gmail.com>
2020-08-20 14:36:37 +0200

    Changes in image visualization

--- ptt/visualization/visualize_imgs.py
+++ ptt/visualization/visualize_imgs.py
@@ -112,7 +112,6 @@ def plot_2d_img(img, save_path=None, figsize=(20, 20)):
     else:
         plt.show()
 
-# TODO: add unnormalization for normalized images
 def visualize_dataloader(dataloader, grid_size=(5, 5), save_path=None, img_size=(512, 512)):
     imgs = get_imgs_from_dataloader(dataloader, grid_size[0]*grid_size[1])
     img_grid = get_img_grid(imgs, grid_size[0], grid_size[1])
@@ -127,16 +126,18 @@ def get_imgs_from_dataloader(dataloader, nr_imgs):
                 imgs.append(img)
         if len(imgs) == nr_imgs:
             break  
-    return imgs  
+    return imgs
 
 import random
 def get_img_grid(img_list, nr_rows, nr_cols, randomize=False):
     if randomize:
         random.shuffle(img_list)
-    img_grid = [[img_list[i+j*nr_cols] for i in range(nr_cols)] for j in range(nr_rows)]
+    img_grid = [[None for i in range(nr_cols)] for j in range(nr_rows)]
+    for j in range(nr_rows):
+        for i in range(nr_cols):
+            if i+j*nr_cols < len(img_list):
+                img_grid[j][i] = img_list[i+j*nr_cols]
     return img_grid
-
-import sys
 
 from PIL import Image
 def create_img_grid(img_grid = [[]], img_size = (512, 512), 
@@ -148,10 +149,10 @@ def create_img_grid(img_grid = [[]], img_size = (512, 512),
     top = margin[1]
     for row in img_grid:
         for img in row:
-            if img.shape[0]==1: # Grayscale
+            if img.shape[0]==1: # Grayscale images
                 img = img[0]
                 img = Image.fromarray(img).resize(img_size)
-            else: # Colored image
+            else: # Colored images
                 if np.argpartition(img.shape, 1)[0] == 0: # If channels first
                     img = np.moveaxis(img, 0, 2) 
                 img = Image.fromarray((img * 255).astype(np.uint8)).resize(img_size).convert('RGB')
@@ -163,3 +164,111 @@ def create_img_grid(img_grid = [[]], img_size = (512, 512),
         new_img.show()
     else:
         new_img.save(save_path)
+
+
+
+import sys
+def get_x_y_from_dataloader(dataloader, nr_imgs):
+    imgs = []
+    for x, y in dataloader:
+        x = x.cpu().detach().numpy()
+        y = y.cpu().detach().numpy()
+        if len(x.shape) == 5: # If each x or y is a batch of volumes
+            # Go from shape (batch, 1, width, height, depth) to 
+            # (batch*depth, 1, width, height) by shifting the depth channel to the
+            # beginning and concatenating all volumes.
+            x_batch = [np.moveaxis(volume_x, -1, 0) for volume_x in x]
+            y_batch = [np.moveaxis(volume_y, -1, 0) for volume_y in y]
+            x = np.concatenate(x_batch)
+            y = np.concatenate(y_batch)
+        assert len(x.shape) == 4
+        for ix, img in enumerate(x):
+            if len(imgs) < nr_imgs:
+                imgs.append((img, y[ix]))
+        if len(imgs) == nr_imgs:
+            break  
+    return imgs
+
+
+def overlay_images(base, overlay, alpha=0.5):
+    """Add transparency to mask, and make composition of image overlayed by 
+    transparent mask.
+    """
+    alpha = int(255*alpha)
+    overlay.putalpha(alpha)
+    return Image.alpha_composite(base, overlay)
+
+def stretch_mask_range(mask):
+    """Stretches the range of mask values to [0, 255] so that they are 
+    differentiable, and converts to RGBA PIL Image.
+    """
+    if mask.max() != 0:
+        mask *= (255.0/mask.max())
+        mask = mask.astype(np.uint8)
+
+
+segmask_colors = {1: {'red': 206, 'green': 24, 'blue': 30},
+    2: {'red': 64, 'green': 201, 'blue': 204},
+    3: {'red': 250, 'green': 245, 'blue': 56},
+    4: {'red': 193, 'green': 69, 'blue': 172},
+    5: {'red': 54, 'green': 71, 'blue': 217}
+    }
+
+def color_mask(mask):
+    """Converts a mask with integer values that are typically < 5 to an RGBA
+    PIL image which each integer is a differentiable color.
+    """
+    mask = mask.astype(np.uint8)
+    mask = np.stack((mask,)*3, axis=-1)
+    red, green, blue = mask.T
+    for seg_value, new_color in segmask_colors.items():
+        to_replace = (red == seg_value) & (blue == seg_value) & (green == seg_value)
+        red[to_replace] = new_color['red']
+        green[to_replace] = new_color['green']
+        blue[to_replace] = new_color['blue']
+    mask = np.array([red, green, blue]).T
+    return mask
+
+def create_x_y_grid(img_grid = [[]], img_size = (512, 512), 
+    margin = (5, 5), background_color = (255, 255, 255, 255), save_path=None):
+    bg_width = len(img_grid[0])*img_size[0] + (len(img_grid[0])+1)*margin[0]
+    bg_height = len(img_grid)*img_size[1] + (len(img_grid)+1)*margin[1]
+    new_img = Image.new('RGBA', (bg_width, bg_height), background_color)
+    left = margin[0]
+    top = margin[1]
+    for row in img_grid:
+        for img_mask_pair in row:
+            if img_mask_pair is not None: # Is None if grid to large for nr. of images
+                img, mask = img_mask_pair
+                if img.shape[0]==1: # Grayscale images
+                    img = img[0]
+                    img = Image.fromarray(img).resize(img_size).convert('RGBA')
+                    # Stretch the mask values between 0 and 255
+                    mask = mask[0]
+                    mask = color_mask(mask)
+                    Image.fromarray(mask)
+                    Image.fromarray(mask).resize(img_size)
+                    mask = Image.fromarray(mask).resize(img_size).convert('RGBA')
+                else: # Colored images
+                    if np.argpartition(img.shape, 1)[0] == 0: # If channels first
+                        img = np.moveaxis(img, 0, 2) 
+                        mask = np.moveaxis(mask, 0, 2) 
+                    img = Image.fromarray((img * 255).astype(np.uint8)).resize(img_size).convert('RGBA')
+                    mask = Image.fromarray((mask * 255).astype(np.uint8)).resize(img_size).convert('RGBA')
+                # Overlay images
+                x_y_img = overlay_images(img, mask)
+                # Paste into original image
+                new_img.paste(x_y_img, (left, top))
+                left += img_size[0] + margin[0]
+        top += img_size[1] + margin[1]
+        left = margin[0]
+    if save_path is None:
+        new_img.show()
+    else:
+        new_img.save(save_path)
+
+def visualize_dataloader_with_masks(dataloader, grid_size=(5,5), save_path=None, img_size=(512, 512)):
+    imgs = get_x_y_from_dataloader(dataloader, grid_size[0]*grid_size[1])
+    img_grid = get_img_grid(imgs, grid_size[0], grid_size[1])
+    create_x_y_grid(img_grid=img_grid, save_path=save_path, img_size=img_size)
+    
